1eeafff156102225db8a926666a9e8bc12a9bfc0
Rajib_T <rajib@yotech.ltd>
2026-06-29 19:14:42 +0600

    first commit

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by MAC01 on 29/06/2026.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack{
+            ProgressView(value: 10, total: 20)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack (alignment: .trailing){
+                    Text("Second Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle().stroke(lineWidth: 10)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Image(systemName: "forward.fill")
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -2,7 +2,7 @@
 //  ScrumdingerApp.swift
 //  Scrumdinger
 //
-//  Created by YOTECH MAC01 on 29/06/2026.
+//  Created by MAC01 on 29/06/2026.
 //
 
 import SwiftUI
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
