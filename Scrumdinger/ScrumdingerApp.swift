81310321f0c51ce45c49808a603bebae66f5c4b0
Rajib_T <rajib@yotech.ltd>
2026-06-29 18:16:00 +0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by YOTECH MAC01 on 29/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
