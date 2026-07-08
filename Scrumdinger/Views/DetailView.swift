803433f948fde82bfc9f9dda6e4b89eae4360b2d
rajibtalukder <rajibtalukder392@gmail.com>
2026-07-08 15:49:33 +0600

    Navigation in SwiftUI

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by YOTECH MAC01 on 08/07/2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
