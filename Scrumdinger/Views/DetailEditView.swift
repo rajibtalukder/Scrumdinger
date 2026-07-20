28f99d104fae38d2b51c8849f323294d512abc83
rajibtalukder <rajibtalukder392@gmail.com>
2026-07-20 14:26:16 +0600

    DetailEditView created

---
+++ Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Rajib on 20/07/2026.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
