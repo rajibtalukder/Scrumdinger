edf2ae3a9dff0f7f763ec32d8438721b0e953763
rajibtalukder <rajibtalukder392@gmail.com>
2026-07-20 14:33:40 +0600

    DetailEditView & DetailView updated

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -10,6 +10,7 @@ import ThemeKit
 
 struct DetailView: View {
     let scrum : DailyScrum
+    @State private var isPresentingEditView = false
     var body: some View {
         List{
             Section(header : Text("MEETING INFO")) {
@@ -34,13 +35,37 @@ struct DetailView: View {
                         .background(scrum.theme.mainColor)
                         .cornerRadius(4)
                 }
-                    .accessibilityElement(children: .combine)
+                .accessibilityElement(children: .combine)
             }
             
             Section(header : Text("ATTENDEES")) {
                 ForEach(scrum.attendees, content: { i in
                     Label(i.name, systemImage: "person")
                 })
+            }
+            .navigationTitle(scrum.title)
+            .toolbar {
+                Button("Edit") {
+                    isPresentingEditView = true
+                }
+            }
+            .sheet(isPresented: $isPresentingEditView) {
+                NavigationStack {
+                    DetailEditView()
+                        .navigationTitle(scrum.title)
+                        .toolbar {
+                            ToolbarItem(placement: .cancellationAction) {
+                                Button("Cancel") {
+                                    isPresentingEditView = false
+                                }
+                            }
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Done") {
+                                    isPresentingEditView = false
+                                }
+                            }
+                        }
+                }
             }
         }
     }
